e7e6a1ae88af820298f325407e301da2b143d8ce
Syconn <49461127+Syconn@users.noreply.github.com>
2026-06-06 04:12:44 -0400

    Item Rendering Fixed

--- common/src/main/java/mod/syconn/svc/mixin/client/ItemInHandLayerMixin.java
+++ common/src/main/java/mod/syconn/svc/mixin/client/ItemInHandLayerMixin.java
@@ -36,9 +36,11 @@ public abstract class ItemInHandLayerMixin<T extends Player, M extends EntityMod
         if (!itemStack.isEmpty()) {
             final BakedModel bakedModel = Minecraft.getInstance().getItemRenderer().getModel(itemStack, livingEntity.level(), livingEntity, livingEntity.getId() + displayContext.ordinal());
             final IModifiedItemRenderer itemRenderer = IModifiedItemRenderer.INSTANCES.get(itemStack.getItem().getClass());
-            if (itemRenderer != null) itemRenderer.render(livingEntity, (HumanoidModel<? extends LivingEntity>) getParentModel(), itemStack, displayContext, arm == HumanoidArm.LEFT, poseStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, bakedModel);
-            Minecraft.getInstance().getItemRenderer().render(itemStack, displayContext, arm == HumanoidArm.LEFT, poseStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, bakedModel);
-            ci.cancel();
+            if (itemRenderer != null) {
+                itemRenderer.render(livingEntity, (HumanoidModel<? extends LivingEntity>) getParentModel(), itemStack, displayContext, arm == HumanoidArm.LEFT, poseStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, bakedModel);
+                Minecraft.getInstance().getItemRenderer().render(itemStack, displayContext, arm == HumanoidArm.LEFT, poseStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, bakedModel);
+                ci.cancel();
+            }
         }
     }
 }
