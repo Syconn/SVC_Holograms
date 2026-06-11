5cff1151d600b3da805cd5081b3647760b969e4f
Syconn <49461127+Syconn@users.noreply.github.com>
2026-06-11 02:06:22 -0400

    Item Mode now supports voice chat

--- common/src/main/java/mod/syconn/svc/server/savedData/extra/CallData.java
+++ common/src/main/java/mod/syconn/svc/server/savedData/extra/CallData.java
@@ -168,9 +168,6 @@ public class CallData {
                     rec.callID = callId;
                     return rec;
                 });
-
-                this.vc.createBlockCall(callId);
-                this.vc.joinBlockCall(callId, owner.playerUUID);
             } else if (owner.type == ReceiverType.ITEM) {
                 this.ITEM_RECEIVERS.computeIfPresent(owner.receiverID, (id, rec) -> {
                     rec.callID = callId;
@@ -178,6 +175,8 @@ public class CallData {
                     return rec;
                 });
             }
+            this.vc.createCall(callId);
+            this.vc.joinCall(callId, owner.playerUUID);
         }
 
         public void connectToCall(UUID callID, Callee callee) {
@@ -194,8 +193,6 @@ public class CallData {
                     rec.callID = callID;
                     return rec;
                 });
-
-                this.vc.joinBlockCall(callID, callee.playerUUID);
             } else if (callee.type == ReceiverType.ITEM) {
                 this.dirty = true;
                 this.ITEM_RECEIVERS.computeIfPresent(callee.receiverID, (id, rec) -> {
@@ -204,6 +201,7 @@ public class CallData {
                     return rec;
                 });
             }
+            this.vc.joinCall(callID, callee.playerUUID);
         }
 
         public void leaveCall(UUID callId, Callee callee) {
@@ -267,7 +265,7 @@ public class CallData {
                 this.dirty = true;
             }
 
-            for (var member : renderMembers.keySet()) this.vc.joinBlockCall(callID, member);
+            for (var member : renderMembers.keySet()) this.vc.joinCall(callID, member);
         }
 
         public void registerBlockReceiver(UUID blockID, WorldPos pos) {
